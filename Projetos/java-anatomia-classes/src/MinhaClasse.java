812992632880b167b6bd9521e5db33e69dc684e8
Altamirodev <123043403+Altamirodev@users.noreply.github.com>
2023-02-01 23:25:10 -0300

    Inclusão da pasta Projetos

---
+++ Projetos/java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,32 @@
+import java.security.PublicKey;
+
+public class MinhaClasse {
+
+    public static void main(String[] args) {
+    //Exemplos de declaração de VARIÁVEIS: Começam pelo TIPO (String, int...), nome e atribuir valores ou não.
+        /*String nome = "Altamiro";
+        int anoFabricacao = 1983;
+        boolean verdadeira = true
+        verdadeira = false;*/
+
+        String primeiroNome = "Altamiro";
+        String segundoNome = "Junior";
+
+        String nomeCompleto = nomeCompleto (primeironome, segundonome);
+        
+        System.out.println( String  nomeCompleto);
+
+
+        
+    //Exemplos de declaração de MÉTODOS: TipoRetorno, NomeObjetivoNoInfinitivo, Parâmetro(s); Todos os 
+    //métodos costumam ser nomeados com VERBOS NO INFINITIVO: calcular, somar, atribuir, formatar, excluir...
+        /*int somar (int numeroUm, numero2);
+        String formatarCep (long cep);*/
+
+        public public static String nomecompleto {String primeiroNome, String segundoNome)
+            return "Resultado do metodo" + primeiroNome.concat (*).concat(concatdoma;)
+            
+        }
+    }
+
+}
